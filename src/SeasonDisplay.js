80a79191510a7c3fff680c635166ab2d1f72c5b2
katia <kcamivo90@gmail.com>
2021-02-20 19:21:52 +0100

    *Displaying current season

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,7 +1,17 @@
 import React from "react";
 
-const SeasonDisplay = ({lat}) => {
-  
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? 'Summer' : 'Winter';
+  }else{
+      return lat > 0 ? 'Winter': 'Summer';
+  }
+};
+
+const SeasonDisplay = ({ lat }) => {
+  const season = getSeason(lat, new Date().getMonth);
+  console.log(season)
+
   return <div>Season Display</div>;
 };
 
