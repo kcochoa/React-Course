bc15bfaa148ed96c4d505daf3ef064171b8a38cd
katia <kcamivo90@gmail.com>
2021-02-20 20:01:08 +0100

    Extracting options to seasonConfig object Add some styles

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,18 +1,36 @@
+import "./SeasonDisplay.css";
 import React from "react";
+
+const seasonConfig = {
+  summer: {
+    text: "Lets hit the beach",
+    iconName: "sun",
+  },
+  winter: {
+    text: "Burrr, It is chilly",
+    iconName: "snowflake",
+  },
+};
 
 const getSeason = (lat, month) => {
   if (month > 2 && month < 9) {
-    return lat > 0 ? 'Summer' : 'Winter';
-  }else{
-      return lat > 0 ? 'Winter': 'Summer';
+    return lat > 0 ? "summer" : "winter";
+  } else {
+    return lat > 0 ? "winter" : "summer";
   }
 };
 
 const SeasonDisplay = ({ lat }) => {
   const season = getSeason(lat, new Date().getMonth);
-  console.log(season)
+  const { text, iconName } = seasonConfig[season];
 
-  return <div>Season Display</div>;
+  return (
+    <div className={`season-display ${season}`}>
+      <i className={`icon-left massive ${iconName} icon`}></i>
+      <h1>{text}</h1>
+      <i className={`icon-right massive ${iconName} icon`}></i>
+    </div>
+  );
 };
 
 export default SeasonDisplay;
